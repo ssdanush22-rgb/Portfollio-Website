019563ddca469b50c1a52a7c3bf3c9f8631ea959
ssdanush22-rgb <ssdanush22@gmail.com>
2026-08-11 01:38:30 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfollio-Website/',
+
   server: {
     port: 5173,
     proxy: {
